69b4d1d64d9e536bae7fa72bc3c858dd9e9ae5e6
Darlinton Luis Siqueira <darlinton2000@gmail.com>
2023-12-13 22:09:44 -0300

    Criando um Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,8 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +12,57 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        DB::table('units')->insert([
+            'name' => 'APT 100',
+            'id_owner' => '1'
+        ]);
+        DB::table('units')->insert([
+            'name' => 'APT 101',
+            'id_owner' => '1'
+        ]);
+        DB::table('units')->insert([
+            'name' => 'APT 200',
+            'id_owner' => '0'
+        ]);
+        DB::table('units')->insert([
+            'name' => 'APT 201',
+            'id_owner' => '0'
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        DB::table('areas')->insert([
+            'allowed' => '1',
+            'title' => 'Academia',
+            'cover' => 'gym.jpg',
+            'days' => '1,2,4,5',
+            'start_time' => '06:00:00',
+            'end_time' => '22:00:00',
+        ]);
+        DB::table('areas')->insert([
+            'allowed' => '1',
+            'title' => 'Piscina',
+            'cover' => 'pool.jpg',
+            'days' => '1,2,3,4,5',
+            'start_time' => '07:00:00',
+            'end_time' => '23:00:00',
+        ]);
+        DB::table('areas')->insert([
+            'allowed' => '1',
+            'title' => 'Churrasqueira',
+            'cover' => 'barbecue.jpg',
+            'days' => '4,5,6',
+            'start_time' => '09:00:00',
+            'end_time' => '23:00:00',
+        ]);
+
+        DB::table('walls')->insert([
+            'title' => 'Título  de Aviso de Teste',
+            'body' => 'Lorem Ipsum bla bla alsajndf0iasndas fsdoaknfd0iap',
+            'datecreated' => '2023-12-13 21:00:00'
+        ]);
+        DB::table('walls')->insert([
+            'title' => 'Alerta Geral para Todos',
+            'body' => 'Bla bla bla bla cuidado bla bla',
+            'datecreated' => '2023-12-13 22:00:00'
+        ]);
     }
 }
