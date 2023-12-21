f9956bf6486c3f58e919f4511026bd60e39a30c3
Darlinton Luis Siqueira <darlinton2000@gmail.com>
2023-12-20 21:35:29 -0300

    Implementado metodo getInfo em UnitController

--- app/Http/Controllers/UnitController.php
+++ app/Http/Controllers/UnitController.php
@@ -2,9 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Unit;
+use App\Models\UnitPeople;
+use App\Models\UnitPet;
+use App\Models\UnitVehicle;
 use Illuminate\Http\Request;
 
 class UnitController extends Controller
 {
-    //
+    /**
+     * Retorna as informações da unidade
+     *
+     * @param int $id
+     * @return string[]
+     */
+    public function getInfo(int $id): array
+    {
+        $array = ['error' => ''];
+
+        $unit = Unit::find($id);
+        if ($unit) {
+            $peoples = UnitPeople::where('id_unit', $id)->get();
+            $vehicles = UnitVehicle::where('id_unit', $id)->get();
+            $pets = UnitPet::where('id_unit', $id)->get();
+
+            foreach ($peoples as $pKey => $pValue) {
+                $peoples[$pKey]['birthdate'] = date('d/m/Y', strtotime($pValue['birthdate']));
+            }
+
+            $array['peoples'] = $peoples;
+            $array['vehicles'] = $vehicles;
+            $array['pets'] = $pets;
+        } else {
+            $array['error'] = 'Propriedade inexistente';
+            return $array;
+        }
+
+        return $array;
+    }
 }
